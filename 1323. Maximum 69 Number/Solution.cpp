c85faaeef7a0e5dc4fe0bce4abef468d9b04c10a
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2022-11-07 21:30:39 +0530

    Create Solution.cpp

---
+++ 1323. Maximum 69 Number/Solution.cpp
@@ -0,0 +1,29 @@
+//LINK:-https://leetcode.com/problems/maximum-69-number/
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        vector<int> digits;
+        int n=num;
+        while(n)
+        {
+            int digit=n%10;
+            digits.push_back(digit);
+            n/=10;
+        }
+        for(int i=digits.size()-1;i>=0;i--)
+        {
+            if(digits[i]==6)
+            {
+                digits[i]=9;
+                break;
+            }
+        }
+        int ans=0;
+        for(int i=digits.size()-1;i>=0;i--)
+        {
+            ans+=digits[i]*pow(10,i);
+        }
+        return ans;
+    }
+};
