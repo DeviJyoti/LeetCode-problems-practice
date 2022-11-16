1c8d06272479a9587065886b01d883bd97de8ec1
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2022-11-16 19:31:32 +0530

    Create Solution.cpp

---
+++ 125. Valid Palindrome/Solution.cpp
@@ -0,0 +1,26 @@
+//LINK:-https://leetcode.com/problems/valid-palindrome/
+
+//solution:-
+
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = size(s);
+        
+        if(n == 1) return true; 
+        string str = "";
+        for(int i=0; i<n; i++) if(isalnum(s[i])) str +=s[i]; 
+        
+        // converting whole string into uppercase
+        transform(str.begin(), str.end(), str.begin(), ::toupper);
+        
+        int i=0,j=size(str)-1;
+        
+        while(i<j) {
+            if(str[i] != str[j]) return false; 
+            i++,j--;
+        }
+        return true;
+    }
+};
