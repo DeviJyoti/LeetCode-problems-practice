93122308114feba5eda5de68f7c9cb968e96f07e
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2022-11-17 22:52:09 +0530

    Update Solution.cpp

--- 496. Next Greater Element I/Solution.cpp
+++ 496. Next Greater Element I/Solution.cpp
@@ -34,3 +34,40 @@ public:
         return ans;
     }
 };
+
+// second method:-
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++)
+        {
+            for(int j=0;j<nums2.size();j++)
+            {
+                if(nums1[i]==nums2[j])
+                {
+                    if(j==nums2.size()-1)
+                    {
+                        ans.push_back(-1);
+                        break;
+                    }
+                     for(int k=j+1;k<nums2.size();k++)
+                        {
+                            if(nums2[k]>nums1[i])
+                            {
+                                ans.push_back(nums2[k]);
+                                break;
+                            }
+                            else if(k==nums2.size()-1 and nums2[k]<=nums1[i])
+                            {
+                                ans.push_back(-1);
+                                    break;
+                            }
+                        }
+                }
+            }
+           
+        }
+        return ans;
+    }
+};
