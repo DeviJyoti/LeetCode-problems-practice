cdcf597c9e0d2f92930904d350c970d2fd72a273
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2022-11-18 22:26:06 +0530

    Update Solution.cpp

--- 263. Ugly Number/Solution.cpp
+++ 263. Ugly Number/Solution.cpp
@@ -10,7 +10,7 @@ public:
         {
             if(n%2 == 0) n = n/2;
             else if(n%3 == 0) n = n/3;
-            else if(n%5==0) n=n/5;
+            else if(n%5 == 0) n = n/5;
             else break;
         }
         return (n==1);
