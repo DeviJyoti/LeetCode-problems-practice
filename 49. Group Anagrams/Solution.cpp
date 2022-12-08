3966963c7b5e174c42c11499ddb0bb662fb2cd68
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2022-12-08 06:16:02 +0530

    Create Solution.cpp

---
+++ 49. Group Anagrams/Solution.cpp
@@ -0,0 +1,22 @@
+//LINK:-https://leetcode.com/problems/group-anagrams/
+//CODE:-
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mymap;
+        int n=strs.size();
+        string temp;
+        for(int i=0;i<n;i++)
+        {
+            temp=strs[i];
+            sort(temp.begin(),temp.end());
+            mymap[temp].push_back(strs[i]);
+        }
+        vector<vector<string>>result;
+        for(auto e : mymap)
+        {
+            result.push_back(e.second);
+        }
+        return result;
+    }
+};
