eb3ee3cb67bab400b0a21a1cd7c9d4d309a34fd4
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2022-12-08 06:17:19 +0530

    Update Solution.cpp

--- 49. Group Anagrams/Solution.cpp
+++ 49. Group Anagrams/Solution.cpp
@@ -3,14 +3,14 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>mymap; //storing original string in value part
+        unordered_map<string,vector<string>>mymap; //for storing original string in value part
         int n=strs.size();
         string temp;
         for(int i=0;i<n;i++)
         {
             temp=strs[i];
             sort(temp.begin(),temp.end());
-            mymap[temp].push_back(strs[i]);
+            mymap[temp].push_back(strs[i]); 
         }
         vector<vector<string>>result;
         for(auto e : mymap)
