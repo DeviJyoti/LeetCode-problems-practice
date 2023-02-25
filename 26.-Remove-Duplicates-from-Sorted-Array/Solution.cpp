ccabc3406aa0883fe25ebe6217a48c2fe038d84a
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-02-25 18:41:18 +0530

    Create Solution.cpp

---
+++ 26.-Remove-Duplicates-from-Sorted-Array/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        if(nums.size()<2)  return nums.size();;
+         int i=0;
+        for(int j=i+1;j<nums.size();j++)
+        {
+            if(nums[j]>=nums[i+1] && nums[j]!=nums[i])
+            {
+                ++i;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1; 
+    }
+    
+};
