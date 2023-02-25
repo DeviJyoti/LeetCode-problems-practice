e76de27e861baefcc001dbda2d28675aa43040af
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-02-25 18:10:16 +0530

    Create Solution.cpp

---
+++ 121.-Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {public: int maxProfit(vector<int>& prices) { 
+int min=INT_MAX,profit=0;
+for(int i=0;i<prices.size();i++)
+{
+  if(prices[i]<min){
+    min=prices[i]; continue;
+}
+if(prices[i]-min>profit) profit=prices[i]-min;
+  
+}
+return profit;
+  }
+ };
