027fd0a658094bc10cb0758479da533157c574fc
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-02-25 18:05:23 +0530

    Create Solution.cpp

---
+++ 287.-Find-the-Duplicate-Number/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums)
+    {
+        map<int,int>mymap;
+        int j=nums.size()-1;
+        for(int i=0;i<(nums.size()+1)/2;i++)
+        {
+            if(mymap[nums[i]]!=0) return nums[i];
+            else
+            {
+               mymap[nums[i]]=1; 
+            }
+            if(mymap[nums[j]]!=0) return nums[j];
+            else
+            {
+                mymap[nums[j]]=1;
+                j--;
+            } 
+        }
+       return 0;
+    }
+	// for github repository link go to my profile.
+};
