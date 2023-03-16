33c23c15278091159c482a04a718131f920bba7c
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-03-16 05:54:28 +0530

    Create Solution.cpp

---
+++ 922. Sort Array By Parity II/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int i=0,j=1;
+        while(i<nums.size() && j<nums.size()){
+            if(nums[i]%2==0){
+                i+=2;    
+            }
+            else if(nums[j]%2!=0){
+               j+=2;
+            }
+            else
+            swap(nums[i],nums[j]);
+        }
+        return nums;
+    }
+};
