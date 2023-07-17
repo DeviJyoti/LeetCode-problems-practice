47085267968d3c48f3f45790e0e1081e60503bf2
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-07-17 18:47:11 +0530

    Create solution.cpp

---
+++ 189. Rotate Array/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+          k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+(nums.size()-k));
+
+         reverse(nums.begin()+(nums.size()-k),nums.end());
+    
+        reverse(nums.begin(),nums.end());
+    }
+};
