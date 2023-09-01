5649c7d101ff3e064a0372f0f7b6b52e3e38659f
Jyoti Devi <108144709+DeviJyoti@users.noreply.github.com>
2023-09-01 20:38:38 +0530

    Update solution.cpp

--- 153. Find Minimum in Rotated Sorted Array/solution.cpp
+++ 153. Find Minimum in Rotated Sorted Array/solution.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
-        // int min=nums[0];
-        // return min;
 
         int ans=INT_MAX;
         int left=0;
